a9af06cda4b6402d8b2a613b6ecf714fd8c77242
Osipov Evgeny <eosipov@htc-cs.ru>
2019-11-04 17:00:34 +0400

    feat: added chat block layout

--- src/client/pages/main/MainPage.tsx
+++ src/client/pages/main/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Chat from './components/Chat';
 
 const MainPage = (): React.ReactElement => (
   <div>
     <Header />
+    <Chat />
   </div>
 );
 
---
+++ src/client/pages/main/components/Chat/Chat.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import styles from './Chat.module.pcss';
+
+const Chat = (): React.ReactElement => {
+  const [message, setMessage] = useState('');
+
+  const handleSendMessageButtonClick = (
+    ev: React.SyntheticEvent<HTMLButtonElement>
+  ) => {
+    ev.preventDefault();
+    console.log(message);
+  };
+
+  const handleMessageInputChange = (
+    ev: React.SyntheticEvent<HTMLInputElement>
+  ) => {
+    const {
+      currentTarget: { value },
+    } = ev;
+    setMessage(value.trim());
+  };
+
+  return (
+    <div className={styles.chatContainer}>
+      <div className={styles.chatMessagesContainer} />
+      <form className={styles.chatInputContainer}>
+        <input
+          onChange={handleMessageInputChange}
+          value={message}
+          className={styles.chatInput}
+          type="text"
+          placeholder="Введите текст"
+        />
+        <button
+          className={styles.sendMessageButton}
+          type="submit"
+          onClick={handleSendMessageButtonClick}
+        >
+          Отправить
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
