52fbb68d868843aa76af882a2ee3e3387f113f61
Osipov Evgeny <eosipov@htc-cs.ru>
2019-11-12 11:06:35 +0400

    feat: added getting geolocation

--- src/client/pages/main/components/MainPanel/MainPanel.tsx
+++ src/client/pages/main/components/MainPanel/MainPanel.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useCurrentPosition } from './MainPanel.hooks';
+
+import styles from './MainPanel.module.pcss';
 
 const MainPanel = (): React.ReactElement => {
-  return <div>213</div>;
+  // const position = useCurrentPosition();
+  const [position, setPosition] = useState<Position['coords'] | null>(null);
+
+  const handleGetPositionButtonClick = () => {
+    window.navigator.geolocation.getCurrentPosition(position =>
+      setPosition(position.coords)
+    );
+  };
+
+  return (
+    <div className={styles.mainPanelContainer}>
+      <button onClick={handleGetPositionButtonClick}>
+        Получить текущую позицию
+      </button>
+      {position && (
+        <React.Fragment>
+          <p>Широта: {position.latitude}</p>
+          <p>Долгота: {position.longitude}</p>
+          <p>Высота: {position.altitude}</p>
+          <p>Точность: {position.accuracy}</p>
+          <p>Точность Высоты: {position.altitudeAccuracy}</p>
+        </React.Fragment>
+      )}
+    </div>
+  );
 };
 
 export default MainPanel;
